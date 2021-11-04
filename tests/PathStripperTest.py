cc414ac6fedaa062a88309e46335d519597340ce
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2021-11-04 18:28:31 +0100

    Add an option to only check the existence of the dir path

---
+++ tests/PathStripperTest.py
@@ -0,0 +1,23 @@
+'''
+Created on 2 nov. 2021
+
+@author: hubert
+'''
+import unittest
+
+
+class PathStripperTest(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testStripPath(self):
+        pass
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testStripPath']
+    unittest.main()
