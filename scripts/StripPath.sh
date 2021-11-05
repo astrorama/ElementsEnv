b345a1ceffab6448dd866721ea2f3bf5b815c2c8
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2021-11-05 17:47:19 +0100

    Add the E_STRIP_NON_EXIST_ONLY environment variable
    This allows to enable the --strip-non-existent-only feature from the
    shell.

--- scripts/StripPath.sh
+++ scripts/StripPath.sh
@@ -1,8 +1,14 @@
-StripPath_tmpfile=`python%(this_python_version)s -m ElementsEnv.PathStripper --shell=sh --mktemp -e PATH -e LD_LIBRARY_PATH -e PYTHONPATH -e HPATH `
+StripPath_extra_args=""
+if [ -n "E_STRIP_NON_EXIST_ONLY" ]; then
+  StripPath_extra_args="--strip-non-exist-only"
+fi
+StripPath_tmpfile=`python%(this_python_version)s -m ElementsEnv.PathStripper ${StripPath_extra_args} --shell=sh --mktemp -e PATH -e LD_LIBRARY_PATH -e PYTHONPATH -e HPATH `
 StripPathStatus="$?"
 if [ "$StripPathStatus" = 0 -a -n "$StripPath_tmpfile" ]; then
   . $StripPath_tmpfile
 fi
 rm -f $StripPath_tmpfile
 unset StripPath_tmpfile
+unset StripPath_extra_args
 unset E_NO_STRIP_PATH
+unset E_STRIP_NON_EXIST_ONLY
